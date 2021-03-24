ff419d2eb9f2696be4b068cfb15f46eda863a13b
Laszlo Pusok <laszlo.pusok@bitrise.io>
2021-03-24 18:25:43 +0100

    Clean uneeded files after updating

--- depmigrate/migrate.go
+++ depmigrate/migrate.go
@@ -2,6 +2,7 @@ package depmigrate
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
@@ -31,16 +32,47 @@ func NewGoModMigrator(projectDir string) (*GoModMigrator, error) {
 	return &GoModMigrator{projectDir: projectDir}, nil
 }
 
-func (m GoModMigrator) ShouldMigrate() bool {
+func (m GoModMigrator) IsGoPathModeStep() bool {
 	goModPath := filepath.Join(m.projectDir, "go.mod")
 	_, err := os.Stat(goModPath)
 
-	return err == nil
+	return err != nil
 }
 
-func (m GoModMigrator) UpdateProjectToGoModules(goBinaryPath string) error {
+func (m GoModMigrator) UpdateProjectToGoModules(goBinaryPath, packageName string) error {
+	initCmd := command.New(goBinaryPath, "mod", "init").SetDir(m.projectDir).SetStdout(os.Stdout).SetStderr(os.Stderr)
+	if packageName != "" {
+		initCmd = command.New(goBinaryPath, "mod", "init", packageName)
+	}
+
+	fmt.Println()
+	log.Infof("$ %s in %s", initCmd.PrintableCommandArgs(), m.projectDir)
+
+	if err := initCmd.Run(); err != nil {
+		if errorutil.IsExitStatusError(err) {
+			return fmt.Errorf("command exited with nonzero status: %v", err)
+		}
+
+		return fmt.Errorf("failed to run command: %v", err)
+	}
+
+	goModPath := filepath.Join(m.projectDir, "go.mod")
+	// goModContents, err := ioutil.ReadFile(goModPath)
+	// if err != nil {
+	// 	return fmt.Errorf("failed to read file (%s): %v", goModPath, err)
+	// }
+
+	// Prevent error:
+	// go mod tidy
+	// go: github.com/Sirupsen/logrus@v1.4.2: parsing go.mod:
+	// module declares its path as: github.com/sirupsen/logrus
+	// 		but was required as: github.com/Sirupsen/logrus
+	// newGoModContents := strings.ReplaceAll(string(goModContents), "github.com/Sirupsen/logrus", "github.com/sirupsen/logrus")
+	// if err := ioutil.WriteFile(goModPath, []byte(newGoModContents), 0600); err != nil {
+	// 	return fmt.Errorf("failed to write file (%s): %v", goModPath, err)
+	// }
+
 	cmds := []*command.Model{
-		command.New(goBinaryPath, "mod", "init"),
 		command.New(goBinaryPath, "mod", "tidy"),
 		command.New(goBinaryPath, "mod", "vendor"),
 	}
@@ -48,12 +80,50 @@ func (m GoModMigrator) UpdateProjectToGoModules(goBinaryPath string) error {
 	for _, cmd := range cmds {
 		cmd.SetDir(m.projectDir).SetStdout(os.Stdout).SetStderr(os.Stderr)
 
-		fmt.Println()
-		log.Infof("$ %s", cmd.PrintableCommandArgs())
+		lsCmd := command.New("tree", ".").SetDir(m.projectDir).SetStdout(os.Stdout).SetStderr(os.Stderr)
+		if err := lsCmd.Run(); err != nil {
+			return err
+		}
 
+		_, err := os.Stat(goModPath)
+		if err != nil {
+			log.Infof("go.mod does not exists: %s", err)
+		} else {
+			log.Infof("go.mod exist at %s", goModPath)
+			data, err := ioutil.ReadFile(goModPath)
+			if err != nil {
+				return err
+			}
+			log.Infof("go.mod contents: %s", data)
+		}
+
+		fmt.Println()
+		log.Infof("$ %s in %s", cmd.PrintableCommandArgs(), m.projectDir)
 		if err := cmd.Run(); err != nil {
 			if errorutil.IsExitStatusError(err) {
 				return fmt.Errorf("command exited with nonzero status: %v", err)
+			}
+
+			return fmt.Errorf("failed to run command: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func (m GoModMigrator) Update() error {
+	cmds := []*command.Model{
+		command.New("go", "get", "-t", "-u", "./..."),
+		command.New("go", "mod", "vendor"),
+	}
+
+	for _, cmd := range cmds {
+		cmd.SetDir(m.projectDir).SetStdout(os.Stdout).SetStderr(os.Stderr)
+
+		log.Infof("$ %s", cmd.PrintableCommandArgs())
+		if err := cmd.Run(); err != nil {
+			if errorutil.IsExitStatusError(err) {
+				return fmt.Errorf("command failed: %v", err)
 			}
 
 			return fmt.Errorf("failed to run command: %v", err)
--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/bitirse-io/go-mod-update/depmigrate"
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/errorutil"
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/stepman/models"
+	"gopkg.in/yaml.v2"
 )
 
 func failf(format string, v ...interface{}) {
@@ -29,9 +36,63 @@ modupdate projectPath`)
 		failf("%s", err)
 	}
 
-	if migrator.ShouldMigrate() {
-		if err := migrator.UpdateProjectToGoModules("go"); err != nil {
-			failf("Failed to update to go modules: ", err)
+	stepYMLPath := filepath.Join(projectDir, "step.yml")
+	stepYML, err := os.ReadFile(stepYMLPath)
+	if err != nil {
+		failf("failed to read file (%s): %v", stepYMLPath, err)
+	}
+
+	var stepModel models.StepModel
+	if err := yaml.Unmarshal(stepYML, &stepModel); err != nil {
+		failf("failed to unmarshal step.yml: %v", err)
+	}
+
+	if stepModel.Toolkit != nil && stepModel.Toolkit.Go != nil {
+		if migrator.IsGoPathModeStep() {
+			if err := migrator.UpdateProjectToGoModules("go", stepModel.Toolkit.Go.PackageName); err != nil {
+				failf("Failed to update to go modules: ", err)
+			}
+
+			for _, file := range []string{"Gopkg.lock", "Gopkg.toml"} {
+				if err := os.Remove(filepath.Join(projectDir, file)); err != nil {
+					log.Warnf("failed to remove file (%s): %v", file, err)
+				}
+			}
+
+			if err := os.RemoveAll(filepath.Join(projectDir, "Godeps")); err != nil {
+				log.Warnf("failed to remove Godeps: %v", err)
+			}
+
+			if err := buildInTmpDir(projectDir); err != nil {
+				failf("%v", err)
+			}
 		}
 	}
+}
+
+func buildInTmpDir(projectDir string) error {
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		return fmt.Errorf("failed to create temp dir: %v", err)
+	}
+
+	if err := command.CopyDir(projectDir, tmpDir, true); err != nil {
+		return fmt.Errorf("failed to copy directory: %v", err)
+	}
+
+	buildCmd := command.New("go", "build").SetDir(tmpDir).SetStderr(os.Stderr).SetStdout(os.Stdout)
+	log.Infof("$ %s", buildCmd.PrintableCommandArgs())
+	if err := buildCmd.Run(); err != nil {
+		if errorutil.IsExitStatusError(err) {
+			failf("go build command failed: %v", err)
+		}
+
+		failf("failed to run go build: %v", err)
+	}
+
+	if err := os.RemoveAll(tmpDir); err != nil {
+		return fmt.Errorf("failed to remove tmp dir: %v", err)
+	}
+
+	return nil
 }
